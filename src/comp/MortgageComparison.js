09d4c4fa6e94b87d8692b45e36f43a59d0218969
Robert Schultheis <=>
2017-10-23 22:57:57 -0600

    renames

--- src/comp/MortgageComparison.js
+++ src/comp/MortgageComparison.js
@@ -11,16 +11,16 @@ class MortgageComparison extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      mort_15: new Mortgage(400000, 0.03250, 15, 350.00),
-      mort_30: new Mortgage(400000, 0.03930, 30, 350.00),
+      mort15: new Mortgage(400000, 0.03250, 15, 350.00),
+      mort30: new Mortgage(400000, 0.03930, 30, 350.00),
 
       loanAmount: 400000,
       propertyTax: 3000,
       insurance: 1200,
 
-      int_rates: {
-        mort_15: 0.03250,
-        mort_30: 0.03930
+      intRates: {
+        mort15: 0.03250,
+        mort30: 0.03930
       }
     };
   }
@@ -45,14 +45,14 @@ class MortgageComparison extends React.Component {
 
   recalcMortgages = () => {
     this.setState({
-      mort_15: new Mortgage(this.state.loanAmount, this.state.int_rates.mort_15, 15, this.totalMonthlyExpenses()),
-      mort_30: new Mortgage(this.state.loanAmount, this.state.int_rates.mort_30, 30, this.totalMonthlyExpenses()),
+      mort15: new Mortgage(this.state.loanAmount, this.state.intRates.mort15, 15, this.totalMonthlyExpenses()),
+      mort30: new Mortgage(this.state.loanAmount, this.state.intRates.mort30, 30, this.totalMonthlyExpenses()),
     })
   }
 
   updateMortgageIntRate = (mortId, newIntRate) => {
-    var newState = { int_rates: this.state.int_rates }
-    newState["int_rates"][mortId] = newIntRate/100
+    var newState = { intRates: this.state.intRates }
+    newState["intRates"][mortId] = newIntRate/100
     this.setState(newState)
     this.recalcMortgages()
   }
@@ -62,12 +62,12 @@ class MortgageComparison extends React.Component {
       <div>
         <LoanAmountDetails updateCommonMortgageInput={this.updateCommonMortgageInput} loanAmount={this.state.loanAmount} />
         <FixedExpenses     updateCommonMortgageInput={this.updateCommonMortgageInput} insurance={this.state.insurance} propertyTax={this.state.propertyTax} />
-        <ComparisonResults />
+        <ComparisonResults mort15/>
         <div className='sideBySideColumn'>
-          <LoanDetails id='mort_15' mortgage={this.state.mort_15} updateMortgageIntRate={this.updateMortgageIntRate} />
+          <LoanDetails id='mort15' mortgage={this.state.mort15} updateMortgageIntRate={this.updateMortgageIntRate} />
         </div>
         <div className='sideBySideColumn'>
-          <LoanDetails id='mort_30' mortgage={this.state.mort_30} updateMortgageIntRate={this.updateMortgageIntRate} />
+          <LoanDetails id='mort30' mortgage={this.state.mort30} updateMortgageIntRate={this.updateMortgageIntRate} />
         </div>
       </div>
     )
